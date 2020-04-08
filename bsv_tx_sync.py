fc3bdbf7292283b11bed8a79da4a0bd2b9634ec8
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-04-08 12:56:56 +0900

    if get_textdata is None, not insert to db

--- bsv_tx_sync.py
+++ bsv_tx_sync.py
@@ -3,6 +3,8 @@ import os
 from pymongo import MongoClient
 from conf.bsv_tx_sync_conf import BsvTxSyncConf
 import bitsv
+from lib.whats_on_chain_lib import WhatsOnChainLib
+import logging
 
 if __name__ == '__main__':
     username = BsvTxSyncConf.username
@@ -12,20 +14,19 @@ if __name__ == '__main__':
     mongo = MongoClient(connectionstr)
 
     addr = "mndvRsa9eyFDpyuUk5bnXwSyrDEZNhq138"
-    record_address = mongo.test.transaction.find({"address": addr})
+    record_address = mongo.test.address.find({"address": addr})
     if record_address.count() == 0:
         mongo.test.address.insert({"address": addr})
 
-    tx_list = mongo.test.transaction.find({"address": addr})
-    if tx_list.count() == 0:
-        network_api = bitsv.network.NetworkAPI(network='test')
-        bsv_transactions = network_api.get_transactions(addr)
-        for txid in bsv_transactions:
-            mongo.test.transaction.insert({"address": addr, "txid": txid})
-
-    tx_list = mongo.test.transaction.find({"address": addr})
-    for item in tx_list:
-        print("txid:" + item["txid"])
+    ## search woc network during xx minutes
+    network_api = bitsv.network.NetworkAPI(network='test')
+    bsv_transactions = network_api.get_transactions(addr)
+    for txid in bsv_transactions:
+        record_tx = mongo.test.transaction.find_one({"address": addr, "txid": txid})
+        if record_tx == None:
+            responseTx = WhatsOnChainLib.get_textdata(txid)
+            if responseTx != None and responseTx.data != "":
+                mongo.test.transaction.insert({"address": addr, "txid": txid})
 
 
 
